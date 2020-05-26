ddeba768450f53f608da743f7448320f19c87ff0
ZhihengChang <zhihengchang602@gmail.com>
2020-05-26 13:57:40 -0400

    EE: Update read command input format. Now supporting 'read filename' for reading image

--- lab1/Easy Edit/cv.py
+++ lab1/Easy Edit/cv.py
@@ -36,12 +36,12 @@ while True:
     #Save Image
     elif cmd.strip().lower() == 's':
         if read:
-            outfilename = util.deafut_output_name
+            outImgName = util.deafut_output_name
             if len(input_array) > 1:
                 if input_array[1] is not None:
-                    outfilename = input_array[1]
+                    outImgName = input_array[1]
             try:
-                cv2.imwrite(outfilename, img)
+                cv2.imwrite(outImgName, img)
             except:
                 print(util.save_failed)
                 print(util.save_failed_note)
@@ -55,9 +55,14 @@ while True:
 
     #Read an Image
     elif cmd.strip().lower() == 'read':
+        readImgName = ''
+        if len(input_array) > 1:
+            readImgName = input_array[1]
+        else:
+            readImgName = input(util.enter_imgname)
+
         try:
-            user_input = input(util.enter_imgname)
-            img = cv2.imread(img_path + user_input, 1)
+            img = cv2.imread(img_path + readImgName, 1)
             read = True
             print(img.shape)
             print(util.read_succeed)
